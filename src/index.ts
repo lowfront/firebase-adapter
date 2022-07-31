129b7c48322f5399b8ea150c8574f9d1f103e131
Low Front <lowfronterior@gmail.com>
2022-07-31 16:49:55 +0900

    update: remove invalid import

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,3 @@ export { default as FirebaseAdapter } from './firebase';
 export { default as FirestoreAdapter } from './firestore';
 export { default } from './firestore';
 export { createFirebaseCustomTokenHandler, createRemoveExpiredSessions } from './next';
-export * from './web';
